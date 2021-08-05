b34954a641c3ac59525fab7560bebdebef30bf04
Emine İnan <emineinanx@gmail.com>
2021-08-05 14:50:34 +0300

    Modify ArtDatabase class

--- app/src/main/java/com/example/artbook/data/ArtDatabase.kt
+++ app/src/main/java/com/example/artbook/data/ArtDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.artbook.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.artbook.data.model.Art
 
 @Database(entities = [Art::class], version = 1)
-abstract class ArtDatabase {
+abstract class ArtDatabase : RoomDatabase() {
     abstract fun artDao(): ArtDao
 }
