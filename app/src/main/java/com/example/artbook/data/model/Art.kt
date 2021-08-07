c5c1da3a4949dfed23918e99678e9a09251c29b5
Emine İnan <emineinanx@gmail.com>
2021-08-07 10:18:11 +0300

    Create ArtViewModel class

--- app/src/main/java/com/example/artbook/data/model/Art.kt
+++ app/src/main/java/com/example/artbook/data/model/Art.kt
@@ -6,10 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "art_table")
 data class Art(
-    @PrimaryKey(autoGenerate = true)
-    var id: Int? = null,
     var artName: String,
     var artistName: String,
     var artYear: Int,
-    var imageUrl: String
+    var imageUrl: String,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
 )
---
+++ app/src/main/java/com/example/artbook/viewmodel/ArtViewModel.kt
@@ -0,0 +1,80 @@
+package com.example.artbook.viewmodel
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.artbook.data.model.Art
+import com.example.artbook.model.ImageResponse
+import com.example.artbook.repo.ArtRepositoryInterface
+import com.example.artbook.util.Resource
+import kotlinx.coroutines.launch
+
+
+class ArtViewModel @ViewModelInject constructor(
+    private val repository: ArtRepositoryInterface
+) : ViewModel() {
+
+    //ArtFragment
+    val artList = repository.getArt()
+
+    //Image API Fragment
+    private val images = MutableLiveData<Resource<ImageResponse>>()
+    val imageList: LiveData<Resource<ImageResponse>>
+        get() = images
+
+    private val selectedImage = MutableLiveData<String>()
+    val selectedImageUrl: LiveData<String>
+        get() = selectedImage
+
+    private var insertArtMsg = MutableLiveData<Resource<Art>>()
+    val insertArtMessage: LiveData<Resource<Art>>
+        get() = insertArtMsg
+
+    fun resetInsertArtMessage() {
+        insertArtMsg = MutableLiveData<Resource<Art>>()
+    }
+
+    fun setSelectedImage(url: String) {
+        selectedImage.postValue(url)
+    }
+
+    fun deleteArt(art: Art) = viewModelScope.launch {
+        repository.deleteArt(art)
+    }
+
+    fun insertArt(art: Art) = viewModelScope.launch {
+        repository.insertArt(art)
+    }
+
+    fun searchForImage(searchString: String) {
+        if (searchString.isEmpty()) {
+            return
+        }
+
+        images.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.searchImage(searchString)
+            images.value = response
+        }
+    }
+
+    fun checkArt(name : String, artistName : String, year : String) {
+        if (name.isEmpty() || artistName.isEmpty() || year.isEmpty() ) {
+            insertArtMsg.postValue(Resource.error("Enter name, artist, year", null))
+            return
+        }
+        val yearInt = try {
+            year.toInt()
+        } catch (e: Exception) {
+            insertArtMsg.postValue(Resource.error("Year should be number",null))
+            return
+        }
+
+        val art = Art(name, artistName, yearInt,selectedImage.value?: "")
+        insertArt(art)
+        setSelectedImage("")
+        insertArtMsg.postValue(Resource.success(art))
+    }
+}
